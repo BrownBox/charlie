c5f694d1e27f88764077abe7ce99463e1779fb97
Mark Parnell <markparnell@brownbox.net.au>
2015-11-27 14:52:29 +1100

    Added support for multiple equalisers on a page

--- js/vendor/bb/equaliser.js
+++ js/vendor/bb/equaliser.js
@@ -1,14 +1,16 @@
 function bb_equaliser() {
-	jQuery('.bb-equaliser').css('height', 'auto');
-    var height = 0;
-    jQuery('.bb-equaliser').each(function() {
-        height = Math.max(height, jQuery(this).height());
+    jQuery('.bb-equaliser').css('height', 'auto');
+    jQuery('.bb-equaliser-wrapper').each(function() {
+    	var height = 0;
+    	jQuery(this).find('.bb-equaliser').each(function() {
+    		height = Math.max(height, jQuery(this).height());
+    	});
+    	jQuery(this).find('.bb-equaliser').height(height);
     });
-    jQuery('.bb-equaliser').height(height);
 }
 jQuery(document).ready(function() {
-	bb_equaliser();
+    bb_equaliser();
 });
 jQuery(window).resize(function() {
-	bb_equaliser();
+    bb_equaliser();
 });
